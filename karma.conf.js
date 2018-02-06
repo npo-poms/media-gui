2c9a88f020f26b9052a9f3fe97a57e63919fa9f6
m.meeuwissen <m.meeuwissen@vpro.nl>
2018-02-06 21:20:19 +0000

    indentation only
    svn path=/nl/vpro/media-gui/trunk/; revision=110463

--- karma.conf.js
+++ karma.conf.js
@@ -1,78 +1,78 @@
 module.exports = function (config) {
 
-	config.set({
+  config.set({
 
-		basePath:'',
+    basePath:'',
 
-		frameworks: ['jasmine'],
+    frameworks: ['jasmine'],
 
-		plugins: [
-			'karma-jasmine',
-			'karma-phantomjs-launcher',
-			'karma-firefox-launcher',
-			'karma-chrome-launcher',
-            'karma-junit-reporter',
-			'karma-coverage'
-		],
+    plugins: [
+      'karma-jasmine',
+      'karma-phantomjs-launcher',
+      'karma-firefox-launcher',
+      'karma-chrome-launcher',
+      'karma-junit-reporter',
+      'karma-coverage'
+    ],
 
-		files: [
-			'test/unit/phantomjs-polyfill.js',
+    files: [
+      'test/unit/phantomjs-polyfill.js',
 
-            'node_modules/angular/angular.js',
-            'node_modules/angular-mocks/angular-mocks.js',
-            'node_modules/lodash/index.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/lodash/index.js',
 
-			'src/js/poms/modules/editor/*.js',
-			'src/js/poms/modules/editor/**/*.js',
+      'src/js/poms/modules/editor/*.js',
+      'src/js/poms/modules/editor/**/*.js',
 
-            'src/js/poms/modules/gtaa/*.js',
-            'src/js/poms/modules/gtaa/**/*.js',
+      'src/js/poms/modules/gtaa/*.js',
+      'src/js/poms/modules/gtaa/**/*.js',
 
-            'src/js/poms/modules/list/*.js',
-			'src/js/poms/modules/list/**/*.js',
+      'src/js/poms/modules/list/*.js',
+      'src/js/poms/modules/list/**/*.js',
 
-            'src/js/poms/modules/media/*.js',
-            'src/js/poms/modules/media/**/*.js',
+      'src/js/poms/modules/media/*.js',
+      'src/js/poms/modules/media/**/*.js',
 
-            'src/js/poms/modules/search/*.js',
-			'src/js/poms/modules/search/**/*.js',
+      'src/js/poms/modules/search/*.js',
+      'src/js/poms/modules/search/**/*.js',
 
-			'test/unit/modules/editor/**/*.js',
-			'test/unit/modules/media/**/*.js',
-			'test/unit/modules/search/**/*.js'
-		],
+      'test/unit/modules/editor/**/*.js',
+      'test/unit/modules/media/**/*.js',
+      'test/unit/modules/search/**/*.js'
+    ],
 
-		exclude: [
-            //'src/js/poms/app.js'
-		],
+    exclude: [
+      //'src/js/poms/app.js'
+    ],
 
-		reporters: [
-			'progress',
-			'coverage'
-		],
+    reporters: [
+      'progress',
+      'coverage'
+    ],
 
-		coverageReporter: {
-			type : 'html',
-			dir : 'coverage/'
-		},
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
-		preprocessors: {
-    		'src/js/poms/**/*.js': ['coverage']
-		},
+    preprocessors: {
+      'src/js/poms/**/*.js': ['coverage']
+    },
 
-		port:9191,
+    port:9191,
 
-		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-		logLevel: config.LOG_DEBUG,
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
 
-		browsers:[
-			'Firefox'
-//			'PhantomJS'
-//			'Chrome'
-		],
+    browsers:[
+      'Firefox'
+      //			'PhantomJS'
+      //			'Chrome'
+    ],
 
-		autowatch: false,
+    autowatch: false,
 
-		singleRun: true
-	});
+    singleRun: true
+  });
 };
