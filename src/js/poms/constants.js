abaa2d9938f7730efe84d9d7d13e108a39059faf
m.meeuwissen <m.meeuwissen@vpro.nl>
2018-02-13 10:23:49 +0000

    Restored.
    svn path=/nl/vpro/media-gui/trunk/; revision=110629

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,4 +1,4 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"apihost":"https://poms-dev.omroep.nl","imagesapihost":"https://poms-dev.omroep.nl/images/api"})
+  .constant('appConfig', {"apihost":"","imagesapihost":"/images/api"})
   .constant('LOCATIONS_API_CONFIG', {"API_URL":"https://rs.vpro.nl/v3/api/","API_KEY":"vprodigitaal","API_SECRET":"tndzOIjEwhxSBO5x","API_ORIGIN":"http://www.vpro.nl","API_PATH":",uri:/v3/api/"});
