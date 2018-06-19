cd84ec3224fd5a6399267c2b5998afa76316d560
m.meeuwissen <m.meeuwissen@vpro.nl>
2018-06-19 12:19:27 +0000

    MSE-4060
    svn path=/nl/vpro/media-gui/trunk/; revision=113199

--- src/js/poms/modules/media/controllers/LocationsController.js
+++ src/js/poms/modules/media/controllers/LocationsController.js
@@ -70,7 +70,7 @@ angular.module( 'poms.media.controllers' ).controller( 'LocationsController', [
                 return this.mediaService.hasWritePermission(this.$scope.media, 'locations');
             },
             mayUpload: function() {
-                return this.$scope.media.avType === 'VIDEO' &&
+                return this.$scope.media.avType && this.$scope.media.avType.id === 'VIDEO' &&
                     this.$scope.media.permissions['LOCATION_UPLOAD'];
             },
 
