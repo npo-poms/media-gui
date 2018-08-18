4dbb15381428dea086d4ad9ede9faa54b746a199
m.meeuwissen <m.meeuwissen@vpro.nl>
2018-08-18 11:40:58 +0000

    Rolled back check in on constants.js
    svn path=/nl/vpro/media-gui/trunk/; revision=114598

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,4 +1,4 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"apihost":"http://eric.vpro.nl:8071","imagesapihost":"http://eric.vpro.nl:8071/images/api"})
+  .constant('appConfig', {"apihost":"","imagesapihost":"/images/api"})
   .constant('LOCATIONS_API_CONFIG', {"API_URL":"https://rs.vpro.nl/v3/api/","API_KEY":"vprodigitaal","API_SECRET":"tndzOIjEwhxSBO5x","API_ORIGIN":"http://www.vpro.nl","API_PATH":",uri:/v3/api/"});
