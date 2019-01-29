5653400d8b62e2e4cd28a5999f2202935f9f197c
m.meeuwissen <m.meeuwissen@vpro.nl>
2019-01-29 12:49:21 +0000

    MSE-4195
    svn path=/nl/vpro/media-gui/trunk/; revision=117593

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,4 +1,3 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"apihost":"","imagesapihost":"/images/api"})
-  .constant('LOCATIONS_API_CONFIG', {"API_URL":"https://rs.vpro.nl/v3/api/","API_KEY":"vprodigitaal","API_SECRET":"tndzOIjEwhxSBO5x","API_ORIGIN":"http://www.vpro.nl","API_PATH":",uri:/v3/api/"});
+  .constant('appConfig', {"apihost":"","imagesapihost":"/images/api"});
