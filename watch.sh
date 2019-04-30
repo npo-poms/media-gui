428116ffce99c8fd29bc0059e7f5f72e9b76f231
m.meeuwissen <m.meeuwissen@vpro.nl>
2019-04-30 15:26:08 +0000

    Trying to support running https.
    svn path=/nl/vpro/media-gui/trunk/; revision=119346

--- watch.sh
+++ watch.sh
@@ -10,17 +10,30 @@
 localhost=`hostname`
 : ${API_PORT:=8071}
 : ${API_SERVER:=$localhost}
-: ${API_HOST:=http://$API_SERVER:$API_PORT}
+: ${API_SCHEME:=http}
+: ${API_HOST:=$API_SCHEME://$API_SERVER:$API_PORT}
+
 
 D=$(dirname $BASH_SOURCE)
 DIR=`(cd $D ; pwd)`
 PATH=$DIR/node:$PATH
 CONSTANTS=$DIR/src/js/poms/constants.js
+ARGS=
+RUN="watch:dev"
+
+if [ $API_SCHEME eq 'https' ] ; then
+  if [ -e "$D/server.key" ] ; then
+    echo "creating key"
+  fi
+  ARGS="SERVER_KEY_PATH=$D/server.key SERVER_CRT_PATH=$D/server.crt"
+  RUN="watch:dev-secure"
+fi
+
 echo "Using Media Backend API host $API_HOST"
 
-echo "Find frontend at http://$localhost:4000"
+echo "Find frontend at $API-SCHEME://$localhost:4000"
 echo "Making backup of $CONSTANTS"
 cp $CONSTANTS $DIR/constants.js.bu
-(cd $DIR ; NPM_CONFIG_APIHOST=$API_HOST NPM_CONFIG_SIGNONHOST=http://sso-dev.omroep.nl npm run watch:dev)
+(cd $DIR ; NPM_CONFIG_APIHOST=$API_HOST NPM_CONFIG_SIGNONHOST=https://sso-dev.omroep.nl $ARGS npm run $RUN)
 echo "Restoring $CONSTANTS"
 mv -f $DIR/constants.js.bu $CONSTANTS
