48f1edb379c4e648921b0e159e901ef50164b659
m.meeuwissen <m.meeuwissen@vpro.nl>
2019-10-18 08:10:48 +0000

    The documentation sais /CMSSelector/media.js. It should  not be changed!!
    svn path=/nl/vpro/media-gui/trunk/; revision=122724

---
+++ src/CMSSelector/media.js
@@ -0,0 +1,52 @@
+const getSrcOrigin = new URL(document.currentScript.src).origin;
+
+var media = {
+
+    select: function ( callback, options ) {
+
+        var popup;
+        var iframe;
+
+        var domain = getSrcOrigin;
+        var query = '';
+
+        if ( options ) {
+            for ( var i in options ) {
+                var value = options[ i ];
+                if ( value !== '' ) {
+                    query += ( query.length ? '&' : '?') + i + '=' + value;
+                }
+            }
+        }
+        function handleMessage ( e ) {
+
+            if ( e.origin === domain ) {
+                callback( e.data );
+                if ( popup ) {
+                    popup.close();
+                }
+                if ( iframe ) {
+                    iframe.parentNode.removeChild( iframe );
+                }
+            }
+        }
+
+        if ( window.addEventListener ) {
+            window.addEventListener( 'message', handleMessage, false );
+        } else if ( window.attachEvent ) {
+            window.attachEvent( 'message', handleMessage, false );
+        }
+
+        if ( document.all ) {
+            // some hackery to help IE, because in IE you may not post to other window, only from an iframe.
+            iframe = document.createElement( 'iframe' );
+            iframe.setAttribute( 'style', 'position:absolute;display:none' );
+            iframe = document.body.appendChild( iframe );
+            iframe.src = domain + '/CMSSelector/IE.html' + query;
+        } else {
+            popup = window.open( domain + '/CMSSelector/' + query, '', 'width=1024,height=800,titlebar=no,toolbar=no,statusbar=no,directories=no,location=no' );
+        }
+
+    }
+};
+
