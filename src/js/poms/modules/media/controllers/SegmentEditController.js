b66d0b87141ccaa25691cfb2259ff7e33a957826
m.meeuwissen <m.meeuwissen@vpro.nl>
2019-11-22 10:02:06 +0000

    MSE-4619 negatieve offset.
    svn path=/nl/vpro/media-gui/trunk/; revision=123466

--- src/js/poms/modules/media/controllers/SegmentEditController.js
+++ src/js/poms/modules/media/controllers/SegmentEditController.js
@@ -26,7 +26,7 @@ angular.module( 'poms.media.controllers' ).controller( 'SegmentEditController', 
             // 00:00:00.000
             // <h> H <m> M <s> S
             // <ms>
-            this.$scope.durationRegexp = /^(\d+:\d{2}(:\d{2})?([\\.,]\d{3})?|(\d+H)?(\d+\s*M)?\s*(\d+(\.\d+)?\s*S)?|\d+|)$/i;
+            this.$scope.durationRegexp = /^-?(\d+:\d{2}(:\d{2})?([\\.,]\d{3})?|(\d+H)?(\d+\s*M)?\s*(\d+(\.\d+)?\s*S)?|\d+|)$/i;
             this.$scope.durationPlaceholder = "00:00:00,000 of 0 H 4 M 1.2 S of 123123";
 
             this.$scope.fieldNames = {
