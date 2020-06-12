0ac4729c5607691a853a0bf0afbf57c0576d3fc3
m.meeuwissen <m.meeuwissen@vpro.nl>
2020-06-12 10:10:01 +0000

    s/.///
    svn path=/nl/vpro/media-gui/trunk/; revision=126690

--- check-correctness.sh
+++ check-correctness.sh
@@ -3,10 +3,10 @@ for i in `find . -name index.html | grep 'deploy/CMSSelector/index.html'`; do
   lines=`cat $i | wc -l`
   echo $lines
   if (( $lines > 1 )); then
-    echo "ERROR CMSSelector.index.html looks ERORNEOUS!!"
+    echo "ERROR CMSSelector/index.html looks ERORNEOUS!!"
     exit 1;
   else
-    echo "CMSSelector.index.html looks fine"
+    echo "CMSSelector/index.html looks fine"
     exit 0;
   fi
 done
