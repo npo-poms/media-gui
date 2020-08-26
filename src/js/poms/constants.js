de247f27252f72f5239c39c9a2db1204dc14b9a5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-08-26 09:18:16 +0200

    Corrected constants.js again.
    only check this in if you made manual changes.

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,3 +1,3 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"version":"5.14.0","apiHost":"https://poms-dev.omroep.nl"});
+  .constant('appConfig', {"apihost":"","imagesapihost":"/images/api"});
