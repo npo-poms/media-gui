26f397c1265d2dccedb51cef120c2ef05eec4684
Michiel Meeuwissen <git@meeuw.org>
2023-10-17 21:45:49 +0200

    moved that.

--- server.dev.js
+++ server.dev.js
@@ -15,7 +15,7 @@ app.use( '/node_modules', express.static( './node_modules' ) );
 app.use( '/js', express.static( './src/js' ) );
 app.use( '/views', express.static( './src/views' ) );
 app.use( '/resources', express.static( './src/resources' ) );
-app.use( '/build', express.static( './build' ) );
+app.use( '/generated-js', express.static( './src/generated-js' ) );
 
 
 app.listen( port, function () {
