a79e656204762ba455d3aaf6c422c1d229c12a8c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-11-01 20:47:51 +0100

    Fixed Genre-editor.

--- src/js/poms/modules/media/directives/PomsEditGenres.js
+++ src/js/poms/modules/media/directives/PomsEditGenres.js
@@ -1,195 +1,197 @@
 angular.module( 'poms.media.directives' )
-    .directive( 'pomsGenres', ['PomsEvents', 'EditService', 'FavoritesService' ,'MediaService', 'ListService', '$q', '$sce', '$timeout', '$uibModal', 'EditFieldService', function ( pomsEvents, editService, favoritesService, mediaService, listService, $q, $sce, $timeout, $uibModal, editFieldService ) {
-        return {
-            restrict: 'E',
-            templateUrl: '/views/edit/editables/poms-genres.html',
-            scope: {
-                media: '=',
-                helpField : '@'
-            },
-            controller: function ( $scope ) {
-
-                function load ( media, dest ) {
-                    mediaService.getGenres( media ).then(
-                        function ( data ) {
-                            dest = angular.copy( data, dest );
-                        },
-                        function ( error ) {
-                            $scope.$emit( pomsEvents.error, error )
-                        }
-                    )
-                }
-                this.$onInit = function() {
-                    const media = $scope.media;
-                    let uiSelect;
-
-                    $scope.genres = [];
-                    $scope.options = [];
-                    $scope.preSelectedItems = [];
-                    $scope.isOpen = false;
-                    $scope.mayWrite = function () {
-                        return mediaService.hasGenrePermission(media);
-                    }.bind(this);
-
-
-                    load(media, $scope.genres);
-
-                    listService.getGenres().then(
-                        function (data) {
-                            $scope.options = data;
-                        },
-                        function (error) {
-                            $scope.$emit(pomsEvents.error, error)
-                        }
-                    );
-                };
-
-
-
-                $scope.$on( 'closeEditField', function ( e, data ) {
-                    if ( data.field === $scope.field ) {
-                        $timeout( function () {
-                            $scope.cancel();
-                        }, 0 );
-                    }
-                } );
-
-                $scope.showEditElement = function ( ) {
-
-                    if ( $scope.mayWrite() && !$scope.isOpen ) {
-
+    .directive( 'pomsGenres', [
+        'PomsEvents',
+        'EditService', 'FavoritesService' ,'MediaService', 'ListService', '$q', '$sce', '$timeout',
+        '$uibModal', 'EditFieldService',
+        function ( pomsEvents, editService, favoritesService, mediaService, listService, $q, $sce, $timeout, $uibModal, editFieldService ) {
+            return {
+                restrict: 'E',
+                templateUrl: '/views/edit/editables/poms-genres.html',
+                scope: {
+                    media: '=',
+                    helpField : '@'
+                },
+                controller: function ( $scope ) {
+                    function load ( media, dest ) {
                         mediaService.getGenres( media ).then(
                             function ( data ) {
-
-                                $scope.selectedItems = {
-                                    selected :[]
-                                };
-
-                                for (let i = 0; i < data.length; i ++ ) {
-                                    for (let j = 0; j < $scope.options.length; j ++ ) {
-                                        if ( $scope.options[j].id === data[i].id ) {
-                                            $scope.selectedItems.selected.push( $scope.options[j] );
-                                        }
-                                    }
-                                }
-
-                                $scope.isOpen = true;
-                                $scope.errorText = false;
-
-                                // We lookup the ui-select element via childscope because we can't acces it by name and dont want to alter its code
-                                uiSelect = $scope.$$childHead.$select;
-                                uiSelect.activate();
-
+                                dest = angular.copy( data, dest );
                             },
                             function ( error ) {
                                 $scope.$emit( pomsEvents.error, error )
                             }
-                        );
-
-                        $scope.$emit( 'editFieldOpen', { 'field': $scope.field, 'isOpen': true} );
-
-
+                        )
                     }
-                };
+                    this.$onInit = function() {
+                        const media = $scope.media;
+                        let uiSelect;
 
-                $scope.keyEvent = function ( e ) {
-
-                    if ( e.keyCode === 27 ) {
-                        $scope.cancel();
-                    }
-
-                };
-
-
-                $scope.cancel = function ( e ) {
-                    if ( e ) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                    }
-
-                    $scope.isOpen = false;
-                    $scope.waiting = false;
-
-                    $scope.$emit( 'editFieldOpen', { 'field': $scope.field, 'isOpen': false} );
-
-                };
-
-                $scope.trustAsHtml = function ( value ) {
-                    return $sce.trustAsHtml( value );
-                };
-
-                $scope.submit = function ( e ) {
-                    $scope.waiting = true;
-
-                    if ( e ) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                    }
-
-                    const data = uiSelect.selected;
-                    const deferred = $q.defer();
-
-                    if ( angular.equals( data, $scope.genres ) ) {
-                        $scope.waiting = false;
+                        $scope.genres = [];
+                        $scope.options = [];
+                        $scope.preSelectedItems = [];
                         $scope.isOpen = false;
-                        return; // no change
-                    }
+                        $scope.mayWrite = function () {
+                            return mediaService.hasGenrePermission(media);
+                        }.bind(this);
 
-                    editService.genres( media, data ).then(
-                        function ( result ) {
-                            angular.copy( result, media );
 
-                            load( media, $scope.genres )
-                            deferred.resolve( false );
-                            $scope.isOpen = false;
-                            $scope.waiting = false;
-                            $scope.errorText = false;
+                        load(media, $scope.genres);
 
-                            $scope.$emit( 'saved' );
-                        },
-                        function ( error ) {
-                            $scope.errorText = error.message;
-
-                            if ( error.violations ) {
-                                for (let violation in  error.violations ) {
-                                    $scope.errorText = error.violations[violation];
-                                    deferred.reject( $scope.errorText );
-                                    break;
-                                }
-                            } else {
-                                $scope.$emit( pomsEvents.error, error );
-                            }
-
-                            $scope.waiting = false;
-                        }
-                    );
-                };
-
-                $scope.blurredSave = function( e ){
-
-                    e.stopPropagation();
-
-                    const data = uiSelect.selected;
-
-                    if ( angular.equals( data, $scope.genres ) ) {
-                        $scope.waiting = false;
-                        $scope.isOpen = false;
-                    }else{
-
-                        editFieldService.saveConfirm().then(
-                            function( result ){
-                                if ( result ){
-                                    $scope.submit();
-                                }
+                        listService.getGenres().then(
+                            function (data) {
+                                $scope.options = data;
                             },
-                            function( error ){
-                                $scope.$emit( pomsEvents.error, error );
+                            function (error) {
+                                $scope.$emit(pomsEvents.error, error)
                             }
-                        ) ;
+                        );
+                    };
+
+
+
+                    $scope.$on( 'closeEditField', function ( e, data ) {
+                        if ( data.field === $scope.field ) {
+                            $timeout( function () {
+                                $scope.cancel();
+                            }, 0 );
+                        }
+                    } );
+
+                    $scope.showEditElement = function ( ) {
+                        if ( $scope.mayWrite() && !$scope.isOpen ) {
+                            mediaService.getGenres($scope.media ).then(
+                                function ( data ) {
+
+                                    $scope.selectedItems = {
+                                        selected :[]
+                                    };
+
+                                    for (let i = 0; i < data.length; i ++ ) {
+                                        for (let j = 0; j < $scope.options.length; j ++ ) {
+                                            if ( $scope.options[j].id === data[i].id ) {
+                                                $scope.selectedItems.selected.push( $scope.options[j] );
+                                            }
+                                        }
+                                    }
+
+                                    $scope.isOpen = true;
+                                    $scope.errorText = false;
+
+                                    // We lookup the ui-select element via childscope because we can't acces it by name and dont want to alter its code
+                                    uiSelect = $scope.$$childHead.$select;
+                                    uiSelect.activate();
+
+                                },
+                                function ( error ) {
+                                    $scope.$emit( pomsEvents.error, error )
+                                }
+                            );
+
+                            $scope.$emit( 'editFieldOpen', { 'field': $scope.field, 'isOpen': true} );
+
+
+                        }
+                    };
+
+                    $scope.keyEvent = function ( e ) {
+
+                        if ( e.keyCode === 27 ) {
+                            $scope.cancel();
+                        }
+
+                    };
+
+
+                    $scope.cancel = function ( e ) {
+                        if ( e ) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                        }
+
+                        $scope.isOpen = false;
+                        $scope.waiting = false;
+
+                        $scope.$emit( 'editFieldOpen', { 'field': $scope.field, 'isOpen': false} );
+
+                    };
+
+                    $scope.trustAsHtml = function ( value ) {
+                        return $sce.trustAsHtml( value );
+                    };
+
+                    $scope.submit = function ( e ) {
+                        $scope.waiting = true;
+
+                        if ( e ) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                        }
+
+                        const data = uiSelect.selected;
+                        const deferred = $q.defer();
+
+                        if ( angular.equals( data, $scope.genres ) ) {
+                            $scope.waiting = false;
+                            $scope.isOpen = false;
+                            return; // no change
+                        }
+
+                        editService.genres( $scope.media, data ).then(
+                            function ( result ) {
+                                angular.copy( result, media );
+
+                                load($scope.media, $scope.genres )
+                                deferred.resolve( false );
+                                $scope.isOpen = false;
+                                $scope.waiting = false;
+                                $scope.errorText = false;
+
+                                $scope.$emit( 'saved' );
+                            },
+                            function ( error ) {
+                                $scope.errorText = error.message;
+
+                                if ( error.violations ) {
+                                    for (let violation in  error.violations ) {
+                                        $scope.errorText = error.violations[violation];
+                                        deferred.reject( $scope.errorText );
+                                        break;
+                                    }
+                                } else {
+                                    $scope.$emit( pomsEvents.error, error );
+                                }
+
+                                $scope.waiting = false;
+                            }
+                        );
+                    };
+
+                    $scope.blurredSave = function( e ){
+
+                        e.stopPropagation();
+
+                        const data = uiSelect.selected;
+
+                        if ( angular.equals( data, $scope.genres ) ) {
+                            $scope.waiting = false;
+                            $scope.isOpen = false;
+                        }else{
+
+                            editFieldService.saveConfirm().then(
+                                function( result ){
+                                    if ( result ){
+                                        $scope.submit();
+                                    }
+                                },
+                                function( error ){
+                                    $scope.$emit( pomsEvents.error, error );
+                                }
+                            ) ;
+
+                        }
 
                     }
-
                 }
-            }
-        };
-    }] );
+            };
+        }]
+    );
