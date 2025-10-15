41b663a7fd9f44cc0e6aebd53d7e8d10b445d656
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-10-15 13:11:56 +0200

    upped version.

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,3 +1,3 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"version":"6.16.0","apiHost":""});
+  .constant('appConfig', {"version":"6.17.0","apiHost":"http://michiel.vpro.nl:8071"});
