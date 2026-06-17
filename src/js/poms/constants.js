a8e97268466cbc16b09abc4a9ba63d951b63818f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-06-17 16:41:14 +0200

    Fixed issue with overlopaping text in georestrictions modal.

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,3 +1,3 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"version":"6.21.0"});
+  .constant('appConfig', {"version":"6.21.0","apiHost":"http://poms-michiel.omroep.nl:8071"});
