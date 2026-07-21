53885236a6068af9977ad5b5bebbca50fb39a2ed
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-07-21 11:32:15 +0200

    Upping.

--- src/js/poms/constants.js
+++ src/js/poms/constants.js
@@ -1,3 +1,3 @@
 'use strict';
 angular.module('poms.constants', [])
-  .constant('appConfig', {"version":"6.21.0","apiHost":"http://poms-michiel.omroep.nl:8071"});
+  .constant('appConfig', {"version":"6.22.0","apiHost":""});
